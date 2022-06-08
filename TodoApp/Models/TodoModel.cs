63a4e2e4f027e22bbc2169a831753ea84e3ef8cd
Семья <@DESKTOP-6ASHT4K>
2022-06-08 20:55:39 +0400

    Add project files.

---
+++ TodoApp/Models/TodoModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Models
+{
+    internal class TodoModel
+    {
+        ///При создании модели TodoModel сразу будет присваиваться текущая дата
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        private bool _isDone;
+
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set { _isDone = value; }    
+        }
+    }
+}
